504fd50f141965be8fd48c2c087c5c8c731f5705
kyanh <kyanh@d11ca801-d22c-4fe2-b6a9-c3a27cf485b6>
2008-10-01 15:03:16 +0000

    good job
    git-svn-id: file:///home/svn/texserver@3 d11ca801-d22c-4fe2-b6a9-c3a27cf485b6

--- index.php
+++ index.php
@@ -5,27 +5,77 @@
 	$tex_stream = trim($tex_stream);
 	$jobname = time();
 	$jobdir = dirname(__FILE__) . "/tmp/";
+	$texdir = dirname(__FILE__) . "/texfiles/";
 
-	function typeset() {
-		global $tex_stream, $jobname, $jobdir;
-		exec("echo $tex_stream | /usr/bin/latex -no-shell-escape--halt-on-error --output-directory=$jobdir --jobname=$jobname", $output, $retval);
-		return array('output' => $output, 'retval' => intval($retval));
+	/* create the tex file from tex_stream */
+	function make_tex_file() {
+		global $tex_stream,$jobname,$jobdir,$texdir;
+
+		$ip = $_SERVER['REMOTE_ADDR'];
+		$stream = "%% ip: $ip time: $jobname %%\n$tex_stream";
+
+		$tmpfname = tempnam($texdir, "$jobname-");
+		$handle = fopen($tmpfname, "w");
+		fwrite($handle, $stream);
+		fclose($handle);
+
+		return $tmpfname;
 	}
 
-	if (!empty($tex_stream)) $typeset = typeset();
+	/* typeset the tex_stream */
+	function typeset() {
+		global $tex_stream, $jobname, $jobdir;
+
+		if (!empty($tex_stream)) {
+			$texfile = make_tex_file();
+
+			exec("/usr/bin/latex -no-shell-escape--halt-on-error --output-directory=$jobdir --jobname=$jobname $texfile", $output, $retval);
+		}
+		else {
+			$output = array('please specify the tex stream');
+			$retval = 255;
+		}
+
+		$output = implode("<br>\n", $output);
+		$output = hilight_log($output);
+
+		$retval = intval($retval);
+		return array('output' => $output, 'retval' => $retval);
+	}
+
+	/* higlight the output */
+	function hilight_log($st) {
+		global $jobdir, $texdir;
+		/* hide something for security reason */
+		$st = str_replace($jobdir, "<font color=\"blue\">JOBDIR::</font>", $st);
+		$st = str_replace($texdir, "<font color=\"blue\">JOBDIR::</font>", $st);
+		$st = preg_replace("/(Output written on .*)/", "<font color=\"#66ccdd\">\\1</font>", $st);
+		$st = preg_replace("/(Transcript written on .*)/", "<font color=\"red\">\\1</font>", $st);
+		return $st;
+	}
+
+	/* typset it now */
+	$typeset = typeset();
 ?>
 <html>
 <head>
 	<title>TeX server</title>
 	<style>
 		#tex_stream {
-			width: 600px;
-			height: 400px;
+			width: 500px;
+			height: 300px;
 			display: block;
+			border: 1px inset #fff;
+			padding: 2px;
 		}
 		#tex_stream:focus {
-			background-color: #cccc66;
+			background-color: #f0f0f0;
 		}
+		#tex_stream:hover {
+			background-color: #f3f3f3;
+			color: #000;
+		}
+
 		input {
 			display: inline;
 			width: 100px;
@@ -35,33 +85,68 @@
 		}
 
 		#warn {
-			width: 592px;
-			color: #fff;
-			background: #000;
-			border: 1px outset #fff;
+			width: 494px;
+			border: 1px inset #fff;
 			margin-bottom: 5px;
 			padding: 2px;
-			text-align: center;
+			text-align: right;
+			color: navy;
+			background-color: #f0f0f0;
+			font-size: 90%;
 		}
 
 		#warn strong {
+			color: blue;
+		}
+
+		#warn:hover {
+			color: #fff;
+			background: #000;
+		}
+
+		#warn:hover strong {
 			color: yellow;
+		}
+
+		#svnid {
+			font-size: 90%;
+			width: 500px;
+			text-align: right;
+			font-style: italic;
 		}
 
 		.clear__ {
 			clear: both;
 		}
+
+		#rice {
+			width: 500px;
+			float: left;
+			margin-right: 1em;
+		}
+		#log {
+			width: 300px;
+			height: 400px;
+			float: left;
+			border: 1px inset #fff;
+			color: black;
+			background-color: #f0f0f0;
+			padding: 2px;
+			font-size: 85%;
+			font-family: monospace;
+			overflow: scroll;
+			white-space: nowrap;
+		}
 	</style>
 </head>
 <body>
+	<div id="svnid">$Id$</div>
 	<div id="warn">
-			This is a personal TeX server.
-			This is a demo/testing server.<br>
-			Use it for fun. <strong>Please don't hack</strong>.
-			<br>
-			Send your feedback to <strong>xkYanh</strong> at GmAil d0t c0m.
-			<br>
-			$Id$
+		This is a personal TeX server.
+		This is a demo/test server.<br>
+		Use it for fun. <strong>Please don't hack</strong>.
+		<br>
+		Send your feedback to <strong>xkYanh</strong> at GmAil d0t c0m.
 	</div>
 	<div class="clear__"></div>
 	<div id="rice">
@@ -72,7 +157,7 @@
 		</form>
 	</div>
 	<div id="log">
-		<?php print $jobname; ?>
+		<?php print $typeset['output']; ?>
 	</div>
 	<div class="clear__"></div>
 </body>
